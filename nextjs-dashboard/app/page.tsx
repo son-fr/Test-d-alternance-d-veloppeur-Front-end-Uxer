fe929ac8c3271ce2d32f8863fa62a1e0390e3bd4
Arthur Jarreau <arthur.jarreau@epitech.eu>
2026-07-30 15:07:44 +0200

    [FIX] update page

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -9,11 +9,44 @@ export default function Home() {
 
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <ButtonGroup>
-            <Button button_shape="left-to-right" disabled={false} text="Click Me" height={50} width={250} position="center_center" items_size={{left_icon: 25, font_size: 25, right_icon: 25, badge: 20}} icons={{left_icon: 'golf', right_icon: 'archive'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Cyan/400', focus: 'Semantic/Rose/600', disabled: 'Brand/Fairway/950'}} font_param={{font:manrope, weight:700, style:'normal'}} shadow='shadow-2xl-down' badge='3'/>
-            <Button button_shape="right-to-left" height={50} width={65} items_size={{left_icon: 25}} icons={{left_icon: 'golf'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Cyan/400'}}/>
+            <Button size="M" disabled={false} button_shape= 'semi-rounded-left-to-right' text="Click Me"   position="center_center" icons={{left_icon: 'golf', right_icon: 'archive'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Cyan/400', focus: 'Semantic/Rose/600', disabled: 'Brand/Fairway/950'}} font_param={{font:manrope, weight:700, style:'normal'}} shadow='shadow-2xl-down' badge='3'/>
+            <Button size="M" disabled={false} button_shape= 'semi-rounded-right-to-left' icons={{left_icon: 'golf'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Yellow/600', disabled: 'Brand/Fairway/950'}}/>
           </ButtonGroup>
-          <Button button_shape="rounded" color={{default: 'Brand/Fairway/600'}}/>
         </div>
+
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <ButtonGroup>
+            <Button size="S" disabled={false} button_shape="semi-rounded-left-to-right" text="Click Me" position="center_center" icons={{left_icon: 'golf', right_icon: 'archive'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Cyan/400', focus: 'Semantic/Rose/600', disabled: 'Brand/Fairway/950'}} font_param={{font:manrope, weight:700, style:'normal'}} shadow='shadow-2xl-down' badge='3'/>
+            <Button size="S" disabled={false} button_shape="semi-rounded-right-to-left" icons={{left_icon: 'golf'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Yellow/600', disabled: 'Brand/Fairway/950'}}/>
+          </ButtonGroup>
+        </div>
+
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <ButtonGroup>
+            <Button size="XS" disabled={false} button_shape="semi-rounded-left-to-right" text="Click Me"   position="center_center" icons={{left_icon: 'golf', right_icon: 'archive'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Cyan/400', focus: 'Semantic/Rose/600', disabled: 'Brand/Fairway/950'}} font_param={{font:manrope, weight:700, style:'normal'}} shadow='shadow-2xl-down' badge='3'/>
+            <Button size="XS" disabled={false} button_shape="semi-rounded-right-to-left" icons={{left_icon: 'golf'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Yellow/600', disabled: 'Brand/Fairway/950'}}/>
+          </ButtonGroup>
+        </div>
+
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <Button border={1} button_shape="semi-rounded" color={{default: 'Brand/Fairway/600', hover: 'Brand/Fairway/800', focus: 'Brand/Fairway/600'}} icons={{left_icon: 'check'}} text="Valider" />
+        </div>
+
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <Button border={1} button_shape="semi-rounded" color={{default: 'Neutral/Grey/100', hover: 'Neutral/Grey/500', focus: 'Neutral/Grey/100'}} icons={{left_icon: 'sliders-horizontal'}} text="Filtrer" badge="3"/>
+        </div>
+
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <ButtonGroup>
+            <Button border={1} button_shape="semi-rounded-left-to-right" color={{default: 'Brand/Hazard/600', hover: 'Brand/Hazard/700', focus: 'Brand/Hazard/800'}} text="Supprimer" />
+            <Button border={1} button_shape="semi-rounded-right-to-left" color={{default: 'Brand/Hazard/600', hover: 'Brand/Hazard/700', focus: 'Brand/Hazard/800'}} icons={{left_icon: 'gear-six'}} />
+          </ButtonGroup>        
+        </div>
+
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <Button border={1} button_shape="semi-rounded" color={{default: 'Neutral/Grey/100', hover: 'Neutral/Grey/500', focus: 'Neutral/Grey/100'}} icons={{left_icon: 'trash'}} />
+        </div>
+        
       </main>
     </div>
   );
