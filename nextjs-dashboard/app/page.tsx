58b2b7fd2aab54a09732352bdd468767a22f3f5f
Arthur Jarreau <arthur.jarreau@epitech.eu>
2026-08-03 11:06:30 +0200

    [ADD] get_icons but still don't work

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -5,46 +5,46 @@ import {inter, lusitana, manrope, space_grotesk, sora} from "@/app/ui/font"
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-indigo-500 dark:bg-gray sm:items-start">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between gap-25 py-32 px-16 bg-indigo-500 dark:bg-gray sm:items-start">
 
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <ButtonGroup>
-            <Button size="M" disabled={false} button_shape= 'semi-rounded-left-to-right' text="Click Me"   position="center_center" icons={{left_icon: 'golf', right_icon: 'archive'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Cyan/400', focus: 'Semantic/Rose/600', disabled: 'Brand/Fairway/950'}} font_param={{font:manrope, weight:700, style:'normal'}} shadow='shadow-2xl-down' badge='3'/>
-            <Button size="M" disabled={false} button_shape= 'semi-rounded-right-to-left' icons={{left_icon: 'golf'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Yellow/600', disabled: 'Brand/Fairway/950'}}/>
+            <Button size="M" disabled={false} button_shape= 'semi-rounded-left-to-right' text="Click Me"   position="center_center" icons={{leading_icon: 'golf', trailing_icon: 'archive'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Cyan/400', focus: 'Semantic/Rose/600', disabled: 'Brand/Fairway/950'}} font_param={{font:inter, weight:400, style:'normal'}} shadow='shadow-2xl-down' badge='3'/>
+            <Button size="M" disabled={false} button_shape= 'semi-rounded-right-to-left' icons={{leading_icon: 'golf'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Yellow/600', disabled: 'Brand/Fairway/950'}}/>
           </ButtonGroup>
         </div>
 
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <ButtonGroup>
-            <Button size="S" disabled={false} button_shape="semi-rounded-left-to-right" text="Click Me" position="center_center" icons={{left_icon: 'golf', right_icon: 'archive'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Cyan/400', focus: 'Semantic/Rose/600', disabled: 'Brand/Fairway/950'}} font_param={{font:manrope, weight:700, style:'normal'}} shadow='shadow-2xl-down' badge='3'/>
-            <Button size="S" disabled={false} button_shape="semi-rounded-right-to-left" icons={{left_icon: 'golf'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Yellow/600', disabled: 'Brand/Fairway/950'}}/>
+            <Button size="S" disabled={false} button_shape="semi-rounded-left-to-right" text="Click Me" position="center_center" icons={{leading_icon: 'golf', trailing_icon: 'archive'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Cyan/400', focus: 'Semantic/Rose/600', disabled: 'Brand/Fairway/950'}} font_param={{font:inter, weight:400, style:'normal'}} shadow='shadow-2xl-down' badge='3'/>
+            <Button size="S" disabled={false} button_shape="semi-rounded-right-to-left" icons={{leading_icon: 'golf'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Yellow/600', disabled: 'Brand/Fairway/950'}}/>
           </ButtonGroup>
         </div>
 
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <ButtonGroup>
-            <Button size="XS" disabled={false} button_shape="semi-rounded-left-to-right" text="Click Me"   position="center_center" icons={{left_icon: 'golf', right_icon: 'archive'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Cyan/400', focus: 'Semantic/Rose/600', disabled: 'Brand/Fairway/950'}} font_param={{font:manrope, weight:700, style:'normal'}} shadow='shadow-2xl-down' badge='3'/>
-            <Button size="XS" disabled={false} button_shape="semi-rounded-right-to-left" icons={{left_icon: 'golf'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Yellow/600', disabled: 'Brand/Fairway/950'}}/>
+            <Button size="XS" disabled={false} button_shape="semi-rounded-left-to-right" text="Click Me"   position="center_center" icons={{leading_icon: 'golf', trailing_icon: 'archive'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Cyan/400', focus: 'Semantic/Rose/600', disabled: 'Brand/Fairway/950'}} font_param={{font:inter, weight:400, style:'normal'}} shadow='shadow-2xl-down' badge='3'/>
+            <Button size="XS" disabled={false} button_shape="semi-rounded-right-to-left" icons={{leading_icon: 'golf'}} border={1} color={{default: 'Neutral/Grey/50',hover: 'Semantic/Yellow/600', disabled: 'Brand/Fairway/950'}}/>
           </ButtonGroup>
         </div>
 
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <Button border={1} button_shape="semi-rounded" color={{default: 'Brand/Fairway/600', hover: 'Brand/Fairway/800', focus: 'Brand/Fairway/600'}} icons={{left_icon: 'check'}} text="Valider" />
+          <Button border={1} button_shape="semi-rounded" color={{default: 'Brand/Fairway/600', hover: 'Brand/Fairway/800', focus: 'Brand/Fairway/600'}} icons={{leading_icon: 'check'}} text="Valider" />
         </div>
 
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <Button border={1} button_shape="semi-rounded" color={{default: 'Neutral/Grey/100', hover: 'Neutral/Grey/500', focus: 'Neutral/Grey/100'}} icons={{left_icon: 'sliders-horizontal'}} text="Filtrer" badge="3"/>
+          <Button border={1} button_shape="semi-rounded" color={{default: 'Neutral/Grey/100', hover: 'Neutral/Grey/500', focus: 'Neutral/Grey/100'}} icons={{leading_icon: 'sliders-horizontal'}} text="Filtrer" badge="3"/>
         </div>
 
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <ButtonGroup>
             <Button border={1} button_shape="semi-rounded-left-to-right" color={{default: 'Brand/Hazard/600', hover: 'Brand/Hazard/700', focus: 'Brand/Hazard/800'}} text="Supprimer" />
-            <Button border={1} button_shape="semi-rounded-right-to-left" color={{default: 'Brand/Hazard/600', hover: 'Brand/Hazard/700', focus: 'Brand/Hazard/800'}} icons={{left_icon: 'gear-six'}} />
+            <Button border={1} button_shape="semi-rounded-right-to-left" color={{default: 'Brand/Hazard/600', hover: 'Brand/Hazard/700', focus: 'Brand/Hazard/800'}} icons={{leading_icon: 'gear-six'}} />
           </ButtonGroup>        
         </div>
 
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <Button border={1} button_shape="semi-rounded" color={{default: 'Neutral/Grey/100', hover: 'Neutral/Grey/500', focus: 'Neutral/Grey/100'}} icons={{left_icon: 'trash'}} />
+          <Button border={1} button_shape="semi-rounded" color={{default: 'Neutral/Grey/100', hover: 'Neutral/Grey/500', focus: 'Neutral/Grey/100'}} icons={{leading_icon: 'trash'}} />
         </div>
         
       </main>
--- nextjs-dashboard/app/props/get_icons.tsx
+++ nextjs-dashboard/app/props/get_icons.tsx
@@ -1,51 +1,348 @@
-import Image from "next/image";
+    import { archive, arrow_counter_clockwise, arrow_down, arrow_left, arrow_right, arrow_square_in, arrow_up_right, arrow_up,
+                at, barcode, basket, bell_simple, book_bookmark, building_apartment, building_office, building, buildings,
+                calendar_dots, camera_slash, camera,
+                caret_down, caret_left, caret_right, caret_up_down, caret_up,
+                chart_bar, chart_pie, chat_text, chats,
+                check_circle_fill, check_circle, check, checks,
+                clock_counter_clockwise, clock, compass_tool, credit_card,
+                database, dots_three_vertical, dots_three, download_simple,
+                envelope, export_icon, eye_slash, eye,
+                file_text, flag_banner_fold, flag_banner, flag_checkered, flag_pennant, flag,
+                gauge, gear_six, gift, globe, golf, gps_fix, gps,
+                heart, house,
+                identification_card, info,
+                lifebuoy, lightning, link, list_bullets, list,
+                magnifying_glass, map_pin_area, map_pin, minus_circle, minus,
+                notches, notepad,
+                package_icon, paper_plane_tilt, paperclip, password,
+                pencil_simple, phone, placeholder_icon, plus_circle, plus,
+                printer, prohibit_inset, prohibit, pulse,
+                question,
+                receipt, rows,
+                scales, scan, shield_check, shopping_cart, sidebar_simple,
+                sign_in, sign_out, sliders_horizontal, sliders, sparkle, stack_simple, stack, star_fill, star, suitcase,
+                table, tag_chevron, tag_simple, tag,
+                text_align_left, text_b, text_italic, text_underline,
+                ticket, traffic_cone, trash, trend_down, trend_up, trophy,
+                upload_simple, user, users_three, users,
+                warning_circle_fill, warning_circle, warning_diamond, warning_octagon, warning,
+                x_circle_fill, x_circle, x } from '@/app/import/icons';
 
-export type Icons_name = 'none' | 'archive' | 
-            'arrow-counter-clockwise' | 'arrow-down' | 'arrow-left' | 'arrow-right' | 'arrow-square-in' | 'arrow-up-right' | 'arrow-up' | 
-            'at' | 'barcode' | 'basket' | 'bell-simple' | 'book-bookmark' | 'building-apartment' | 'building-office' | 'building' | 'buildings' | 
-            'calendar-dots' | 'camera-slash' | 'camera' | 
-            'caret-down' | 'caret-left' | 'caret-right' | 'caret-up-down' | 'caret-up' | 
-            'chart-bar' | 'chart-pie' | 'chat-text' | 'chats' | 
-            'check-circle-fill' | 'check-circle' | 'check' | 'checks' | 
-            'clock-counter-clockwise' | 'clock' | 'compass-tool' | 'credit-card' | 
-            'database' | 'dots-three-vertical' | 'dots-three' | 'download-simple' | 
-            'envelope' | 'export' | 'eye-slash' | 'eye' | 
-            'file-text' | 'flag-banner-fold' | 'flag-banner' | 'flag-checkered' | 'flag-pennant' | 'flag' | 
-            'gauge' | 'gear-six' | 'gift' | 'globe' | 'golf' | 'gps-fix' | 'gps' | 
-            'heart' | 'house' | 
-            'identification-card' | 'info' | 
-            'lifebuoy' | 'lightning' | 'link' | 'list-bullets' | 'list' | 
-            'magnifying-glass' | 'map-pin-area' | 'map-pin' | 'minus-circle' | 'minus' | 
-            'notches' | 'notepad' | 
-            'package' | 'paper-plane-tilt' | 'paperclip' | 'password' | 
-            'pencil-simple' | 'phone' | 'Placeholder icon' | 'plus-circle' | 'plus' | 
-            'printer' | 'prohibit-inset' | 'prohibit' | 'pulse' | 
-            'question' | 
-            'receipt' | 'rows' | 
-            'scales' | 'scan' | 'shield-check' | 'shopping-cart' | 'sidebar-simple' | 
-            'sign-in' | 'sign-out' | 'sliders-horizontal' | 'sliders' | 'sparkle' | 'stack-simple' | 'stack' | 'star-fill' | 'star' | 'suitcase' | 
-            'table' | 'tag-chevron' | 'tag-simple' | 'tag' | 
-            'text-align-left' | 'text-b' | 'text-italic' | 'text-underline' | 
-            'ticket' | 'traffic-cone' | 'trash' | 'trend-down' | 'trend-up' | 'trophy' | 
-            'upload-simple' | 'user' | 'users-three' | 'users' | 
-            'warning-circle-fill' | 'warning-circle' | 'warning-diamond' | 'warning-octagon' | 'warning' | 
-            'x-circle-fill' | 'x-circle-fill' | 'x-circle' | 'x';
+    import Image from "next/image";
 
-export default function Draw_Icons(icon: Icons_name | undefined, size: number | undefined) {
-    const Icon_path : string = '/icons/' + icon + '.svg'
-    const Icon_alt : string = icon + 'icon'
+    export type Icons_name = 'none' | 'archive' | 
+                'arrow_counter_clockwise' | 'arrow_down' | 'arrow_left' | 'arrow_right' | 'arrow_square_in' | 'arrow_up_right' | 'arrow_up' | 
+                'at' | 'barcode' | 'basket' | 'bell_simple' | 'book_bookmark' | 'building_apartment' | 'building_office' | 'building' | 'buildings' | 
+                'calendar_dots' | 'camera_slash' | 'camera' | 
+                'caret_down' | 'caret_left' | 'caret_right' | 'caret_up_down' | 'caret_up' | 
+                'chart_bar' | 'chart_pie' | 'chat_text' | 'chats' | 
+                'check_circle_fill' | 'check_circle' | 'check' | 'checks' | 
+                'clock_counter_clockwise' | 'clock' | 'compass_tool' | 'credit_card' | 
+                'database' | 'dots_three_vertical' | 'dots_three' | 'download_simple' | 
+                'envelope' | 'export_icon' | 'eye_slash' | 'eye' | 
+                'file_text' | 'flag_banner_fold' | 'flag_banner' | 'flag_checkered' | 'flag_pennant' | 'flag' | 
+                'gauge' | 'gear_six' | 'gift' | 'globe' | 'golf' | 'gps_fix' | 'gps' | 
+                'heart' | 'house' | 
+                'identification_card' | 'info' | 
+                'lifebuoy' | 'lightning' | 'link' | 'list_bullets' | 'list' | 
+                'magnifying_glass' | 'map_pin_area' | 'map_pin' | 'minus_circle' | 'minus' | 
+                'notches' | 'notepad' | 
+                'package_icon' | 'paper_plane_tilt' | 'paperclip' | 'password' | 
+                'pencil_simple' | 'phone' | 'placeholder_icon' | 'plus_circle' | 'plus' | 
+                'printer' | 'prohibit_inset' | 'prohibit' | 'pulse' | 
+                'question' | 
+                'receipt' | 'rows' | 
+                'scales' | 'scan' | 'shield_check' | 'shopping_cart' | 'sidebar_simple' | 
+                'sign_in' | 'sign_out' | 'sliders_horizontal' | 'sliders' | 'sparkle' | 'stack_simple' | 'stack' | 'star_fill' | 'star' | 'suitcase' | 
+                'table' | 'tag_chevron' | 'tag_simple' | 'tag' | 
+                'text_align_left' | 'text_b' | 'text_italic' | 'text_underline' | 
+                'ticket' | 'traffic_cone' | 'trash' | 'trend_down' | 'trend_up' | 'trophy' | 
+                'upload_simple' | 'user' | 'users_three' | 'users' | 
+                'warning_circle_fill' | 'warning_circle' | 'warning_diamond' | 'warning_octagon' | 'warning' | 
+                'x_circle_fill' | 'x_circle' | 'x';
 
-    if (icon != 'none' && icon != undefined) {
+function get_Icons(name: Icons_name | undefined) {
+    if (!name)
+        return null
+    switch (name) {
+        case 'archive':
+            return archive
+        case 'arrow_counter_clockwise':
+            return arrow_counter_clockwise
+        case 'arrow_down':
+            return arrow_down
+        case 'arrow_left':
+            return arrow_left
+        case 'arrow_right':
+            return arrow_right
+        case 'arrow_square_in':
+            return arrow_square_in
+        case 'arrow_up_right':
+            return arrow_up_right
+        case 'arrow_up':
+            return arrow_up
+        case 'at':
+            return at
+        case 'barcode':
+            return barcode
+        case 'basket':
+            return basket
+        case 'bell_simple':
+            return bell_simple
+        case 'book_bookmark':
+            return book_bookmark
+        case 'building_apartment':
+            return building_apartment
+        case 'building_office':
+            return building_office
+        case 'building':
+            return building
+        case 'buildings':
+            return buildings
+        case 'calendar_dots':
+            return calendar_dots
+        case 'camera_slash':
+            return camera_slash
+        case 'camera':
+            return camera
+        case 'caret_down':
+            return caret_down
+        case 'caret_left':
+            return caret_left
+        case 'caret_right':
+            return caret_right
+        case 'caret_up_down':
+            return caret_up_down
+        case 'caret_up':
+            return caret_up
+        case 'chart_bar':
+            return chart_bar
+        case 'chart_pie':
+            return chart_pie
+        case 'chat_text':
+            return chat_text
+        case 'chats':
+            return chats
+        case 'check_circle_fill':
+            return check_circle_fill
+        case 'check_circle':
+            return check_circle
+        case 'check':
+            return check
+        case 'checks':
+            return checks
+        case 'clock_counter_clockwise':
+            return clock_counter_clockwise
+        case 'clock':
+            return clock
+        case 'compass_tool':
+            return compass_tool
+        case 'credit_card':
+            return credit_card
+        case 'database':
+            return database
+        case 'dots_three_vertical':
+            return dots_three_vertical
+        case 'dots_three':
+            return dots_three
+        case 'download_simple':
+            return download_simple
+        case 'envelope':
+            return envelope
+        case 'export_icon':
+            return export_icon
+        case 'eye_slash':
+            return eye_slash
+        case 'eye':
+            return eye
+        case 'file_text':
+            return file_text
+        case 'flag_banner_fold':
+            return flag_banner_fold
+        case 'flag_banner':
+            return flag_banner
+        case 'flag_checkered':
+            return flag_checkered
+        case 'flag_pennant':
+            return flag_pennant
+        case 'flag':
+            return flag
+        case 'gauge':
+            return gauge
+        case 'gear_six':
+            return gear_six
+        case 'gift':
+            return gift
+        case 'globe':
+            return globe
+        case 'golf':
+            return golf
+        case 'gps_fix':
+            return gps_fix
+        case 'gps':
+            return gps
+        case 'heart':
+            return heart
+        case 'house':
+            return house
+        case 'identification_card':
+            return identification_card
+        case 'info':
+            return info
+        case 'lifebuoy':
+            return lifebuoy
+        case 'lightning':
+            return lightning
+        case 'link':
+            return link
+        case 'list_bullets':
+            return list_bullets
+        case 'list':
+            return list
+        case 'magnifying_glass':
+            return magnifying_glass
+        case 'map_pin_area':
+            return map_pin_area
+        case 'map_pin':
+            return map_pin
+        case 'minus_circle':
+            return minus_circle
+        case 'minus':
+            return minus
+        case 'notches':
+            return notches
+        case 'notepad':
+            return notepad
+        case 'package_icon':
+            return package_icon
+        case 'paper_plane_tilt':
+            return paper_plane_tilt
+        case 'paperclip':
+            return paperclip
+        case 'password':
+            return password
+        case 'pencil_simple':
+            return pencil_simple
+        case 'phone':
+            return phone
+        case 'placeholder_icon':
+            return placeholder_icon
+        case 'plus_circle':
+            return plus_circle
+        case 'plus':
+            return plus
+        case 'printer':
+            return printer
+        case 'prohibit_inset':
+            return prohibit_inset
+        case 'prohibit':
+            return prohibit
+        case 'pulse':
+            return pulse
+        case 'question':
+            return question
+        case 'receipt':
+            return receipt
+        case 'rows':
+            return rows
+        case 'scales':
+            return scales
+        case 'scan':
+            return scan
+        case 'shield_check':
+            return shield_check
+        case 'shopping_cart':
+            return shopping_cart
+        case 'sidebar_simple':
+            return sidebar_simple
+        case 'sign_in':
+            return sign_in
+        case 'sign_out':
+            return sign_out
+        case 'sliders_horizontal':
+            return sliders_horizontal
+        case 'sliders':
+            return sliders
+        case 'sparkle':
+            return sparkle
+        case 'stack_simple':
+            return stack_simple
+        case 'stack':
+            return stack
+        case 'star_fill':
+            return star_fill
+        case 'star':
+            return star
+        case 'suitcase':
+            return suitcase
+        case 'table':
+            return table
+        case 'tag_chevron':
+            return tag_chevron
+        case 'tag_simple':
+            return tag_simple
+        case 'tag':
+            return tag
+        case 'text_align_left':
+            return text_align_left
+        case 'text_b':
+            return text_b
+        case 'text_italic':
+            return text_italic
+        case 'text_underline':
+            return text_underline
+        case 'ticket':
+            return ticket
+        case 'traffic_cone':
+            return traffic_cone
+        case 'trash':
+            return trash
+        case 'trend_down':
+            return trend_down
+        case 'trend_up':
+            return trend_up
+        case 'trophy':
+            return trophy
+        case 'upload_simple':
+            return upload_simple
+        case 'user':
+            return user
+        case 'users_three':
+            return users_three
+        case 'users':
+            return users
+        case 'warning_circle_fill':
+            return warning_circle_fill
+        case 'warning_circle':
+            return warning_circle
+        case 'warning_diamond':
+            return warning_diamond
+        case 'warning_octagon':
+            return warning_octagon
+        case 'warning':
+            return warning
+        case 'x_circle_fill':
+            return x_circle_fill
+        case 'x_circle':
+            return x_circle
+        case 'x':
+            return x
+        default:
+            return null
+    }
+}
+
+export default function Draw_Icons(colors = '#fafafa', icon: Icons_name | undefined, size: number | undefined) {
+        const Icon_path : string = '/icons/' + icon + '.svg'
+        const Icon_alt : string = icon + 'icon'
+        const Icon = get_Icons(icon);
+
+        if (icon != 'none' && icon != undefined) {
+            return(
+            <Icon width={size} height={size} style={{ color: colors }} / >
+            )
+        }
         return(
-        <Image
-            src= {Icon_path}
-            alt= {Icon_alt}
-            width={size}
-            height={size}
-        />
+            null
         )
     }
-    return(
-        null
-    )
-}
