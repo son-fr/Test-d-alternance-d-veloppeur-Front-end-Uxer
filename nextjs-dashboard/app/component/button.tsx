0b1b33b754731853836a2a941959b7c8db0f8633
Arthur Jarreau <arthur.jarreau@epitech.eu>
2026-08-05 15:51:32 +0200

    [FIX] parameters of the button and iconbutton

--- nextjs-dashboard/app/component/button.tsx
+++ nextjs-dashboard/app/component/button.tsx
@@ -2,14 +2,14 @@ import styles from "@/app/component/button.module.css";
 import Get_position, {Item_position} from "@/app/props/get_position";
 import Draw_Icons, {Icons_name} from "@/app/props/get_icons";
 import Get_shape from "@/app/props/get_shape";
-import Get_shadow, {Shadows} from "@/app/props/get_shadow";
-import Get_sizes, {Sizes} from "@/app/props/get_sizes";
-import Get_style, {Styles} from "@/app/props/get_style";
+import Get_sizes from "@/app/props/get_sizes";
+import Get_style from "@/app/props/get_style";
 import {inter, lusitana, manrope, space_grotesk, sora} from "@/app/ui/font"
+import { ColorsValues } from "@/app/props/get_colors";
 
 type Font_param = {
     font?: typeof inter | typeof lusitana | typeof manrope | typeof space_grotesk | typeof sora;
-    weight?: 400 | 700;
+    weight?: 400 | 500 | 600 | 700 ;
     style?: 'normal' | 'italic';
 }
 
@@ -23,52 +23,36 @@ export type Button_props = {
     text?: string;
     size?: 'M' | 'S' | 'XS'; 
     font_param?: Font_param;
+    badge_font_param?: Font_param;
     style?: 'Primary' | 'Secondary' | 'Ghost' | 'Destructive';
     position?:  'left_up' | 'center_up' | 'right_up' | 
                 'left_center' | 'center_center' | 'right_center' | 
                 'left_bottom' | 'center_bottom' | 'right_bottom';
     icons?: Icons;
-    button_shape?: 'rectangle' | 'rounded' | 'semi-rounded' | 'feather' | 'left-to-right' | 'right-to-left' | 'semi-rounded-left-to-right' | 'semi-rounded-right-to-left';
-    shadow?: Shadows;
+    button_shape?: 'rectangle' | 'rounded' | 'semi-rounded' | 'fetaher' | 'left-to-right' | 'right-to-left' | 'semi-rounded-left-to-right' | 'semi-rounded-right-to-left';
+
     border?: number;
     badge?: string;
 }
 
-/**
- * Show a personalisable button.
- *
- * @param {boolean} disabled - Choose if the button is disabled or not. (default: false)
- * @param {string} text - The text of the button. (default: empty)
- * @param {Font_param} font_param - The font of the text, with is weight and style. (default: {
-                                                                                                font: inter,
-                                                                                                weight: 400,
-                                                                                                style: 'normal',
-                                                                                            }, )
- * @param {Colors_state} color - The color of the button on 4 state. (default: {
-                                                                        default : 'none',
-                                                                        hover: 'none',
-                                                                        focus: 'none',
-                                                                        disabled: 'none',
-                                                                    }, )
- * @param {'left_up' | 'center_up' | 'right_up' | 
- *         'left_center' | 'center_center' | 'right_center' | 
- *         'left_bottom' | 'center_bottom' | 'right_bottom'} position - The position of the text, first value position align ( ↔ ), second value position justify ( ↕ ). (default: "center_center")
- * @param {Icons} icons - The icons chose on the left or on the right of the button (chose between all the icons in /icons/ only need the name of the file). (default: {
-                                                                                                                                                                            left_icon: 'none', 
-                                                                                                                                                                            right_icon: 'none',
-                                                                                                                                                                        })
- * @param {'rectangle' | 'rounded' | 'semi-rounded' | 'feather' | 'left-to-right' | 'right-to-left' | 'semi-rounded-left-to-right' | 'semi-rounded-right-to-left'} button_shape - The shape of the button between 5 deiferente shape (rectangle, rounded, feather). (default: "rounded")
- * @param {Shadows} shadow - Define the effect of the button. (default: 'none') 
- * @param {number} border - The border of the button (in px). (default: 0)
- * @param {string} badge - chose the texte inside the badge. (default: empty)
- * @returns The button component.
- */
-
+export type Button_icon_props = {
+    disabled?: boolean;
+    size?: 'M' | 'S' | 'XS';
+    style?: 'Primary' | 'Secondary' | 'Ghost' | 'Destructive' | 'Alpha_Dark' | 'Alpha_Light';
+    icon?: Icons_name;
+    button_shape?: 'rectangle' | 'rounded' | 'semi-rounded' | 'feather' | 'left-to-right' | 'right-to-left' | 'semi-rounded-left-to-right' | 'semi-rounded-right-to-left';
+}
 export default function Button({disabled = false,
                                 size = 'XS',
-                                text = undefined, font_param = {
+                                text = undefined, 
+                                font_param = {
                                             font: inter,
-                                            weight: 400,
+                                            weight: 500,
+                                            style: 'normal',
+                                        },
+                                badge_font_param = {
+                                            font: inter,
+                                            weight: 600,
                                             style: 'normal',
                                         },
                                 style = 'Primary',
@@ -77,11 +61,10 @@ export default function Button({disabled = false,
                                             leading_icon: 'none', 
                                             trailing_icon: 'none',
                                         }, 
-                                button_shape = 'rounded', shadow = 'none', border = 0,
+                                button_shape = 'semi-rounded',
                                 badge = undefined} : Button_props) {
     const Item_position : Item_position = Get_position(position);
     const Shape_button : string = Get_shape(button_shape);
-    const Shadow = Get_shadow(shadow);
     const Sizes = Get_sizes(size);
     const Style = Get_style(style);
  
@@ -90,37 +73,62 @@ export default function Button({disabled = false,
                         gap: `${Sizes.gap}px`, padding: `${Sizes.vertical_padding}px ${Sizes.horizontal_padding}px ${Sizes.vertical_padding}px ${Sizes.horizontal_padding}px`,
                         fontSize: Sizes.text_size, fontWeight: font_param.weight, fontStyle: font_param.style,
                         "--btn-color-disabled": `${Style.colors_state.disabled}`, "--btn-color": `${Style.colors_state.default}`, "--btn-color-hover": `${Style.colors_state.hover}`, "--btn-color-focus": `${Style.colors_state.focus}`,
+                        "--btn-effect-disabled": `${Style.effect.disabled}`, "--btn-effect": `${Style.effect.default}`, "--btn-effect-hover": `${Style.effect.hover}`, "--btn-effect-focus": `${Style.effect.focus}`,
+                        "--btn-opacity-disabled": `${Style.opacity.disabled}`, "--btn-opacity": `${Style.opacity.default}`, "--btn-opacity-hover": `${Style.opacity.hover}`, "--btn-opacity-focus": `${Style.opacity.focus}`,
                         alignItems: Item_position.align, justifyContent: Item_position.justify, 
-                        borderRadius: Shape_button, boxShadow: Shadow, stroke: `${Style.stroke}`, color: `${Style.colors_text}` } as React.CSSProperties} 
-                        className={`${font_param.font.className} ${styles.button} flex whitespace-nowrap gap-2 px-5 text-background`}>
+                        borderRadius: Shape_button, border: `1px solid ${Style.stroke}`, color: `${Style.colors_text}` } as React.CSSProperties} 
+                        className={`${font_param.font.className} ${styles.button} flex whitespace-nowrap gap-2 px-5`}>
             {Draw_Icons(Style.colors_icons, icons.leading_icon, Sizes.icons_size)} 
             {text}
             {Draw_Icons(Style.colors_icons, icons.trailing_icon, Sizes.icons_size)}
-            {Badge(badge, Sizes.badge_text_size, Sizes.badge_size)}
+            {Badge(badge, Sizes.badge_text_size, badge_font_param, Sizes.badge_size, Style.colors_badge)}
         </button>
     )
 }
 
-export function ButtonGroup({children} : any) {
-    return (
-        <div className="flex gap-4px">
-            {children}
-        </div>
-    )
-}
-
-function Badge(badge: string | undefined, text_size: number | undefined, size: number | undefined) {
+function Badge(badge: string | undefined, text_size: number | undefined, badge_font_param: Font_param, size: number | undefined, color: ColorsValues) {
     if (badge != undefined && text_size != undefined) {
         return(
         <div style={{   fontSize: `${text_size}px`, 
+                        fontWeight: badge_font_param.weight, 
+                        fontStyle: badge_font_param.style,
                         height: `${size}px`, width: `auto`, 
-                        backgroundColor: '#a3a3a3'}}
-            className={`flex rounded-[4px] items-center gap-[8px] px-[5px] py-[8px] whitespace-nowrap`}>
+                        backgroundColor: color}}
+            className={`${badge_font_param.font.className} flex rounded-[4px] items-center gap-[8px] px-[5px] py-[8px] whitespace-nowrap`}>
             {badge}
         </div>
         )
     }
     return (
         null
+    )
+}
+
+export function ButtonGroup({children} : any) {
+    return (
+        <div className="flex gap-1px">
+            {children}
+        </div>
+    )
+}
+
+
+export function Button_icon( {icon = 'none',
+                            disabled = false,
+                                style = 'Primary',
+                                button_shape = 'semi-rounded',} : Button_icon_props) {
+    const Shape_button : string = Get_shape(button_shape);
+    const Style = Get_style(style); 
+ 
+    return (
+        <button disabled={disabled} style={{ height: `40px`, width: `auto`,
+                        gap: `8px`, padding: `10px 10px 10px 10px`,
+                        "--btn-color-disabled": `${Style.colors_state.disabled}`, "--btn-color": `${Style.colors_state.default}`, "--btn-color-hover": `${Style.colors_state.hover}`, "--btn-color-focus": `${Style.colors_state.focus}`,
+                        "--btn-effect-disabled": `${Style.effect.disabled}`, "--btn-effect": `${Style.effect.default}`, "--btn-effect-hover": `${Style.effect.hover}`, "--btn-effect-focus": `${Style.effect.focus}`,
+                        "--btn-opacity-disabled": `${Style.opacity.disabled}`, "--btn-opacity": `${Style.opacity.default}`, "--btn-opacity-hover": `${Style.opacity.hover}`, "--btn-opacity-focus": `${Style.opacity.focus}`,
+                        borderRadius: Shape_button, border: `1px solid ${Style.stroke}` } as React.CSSProperties} 
+                        className={`${styles.button} flex whitespace-nowrap gap-2 px-5`}>
+            {Draw_Icons(Style.colors_icons, icon, 20)} 
+        </button>
     )
 }
--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,50 +1,50 @@
 import Image from "next/image";
-import Button, {ButtonGroup} from "./component/button";
+import Button, {ButtonGroup, Button_icon} from "./component/button";
 import {inter, lusitana, manrope, space_grotesk, sora} from "@/app/ui/font"
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between gap-25 py-32 px-16 bg-indigo-500 dark:bg-gray sm:items-start">
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between gap-25 py-32 px-16 bg-[#f5f5f5]  sm:items-start">
 
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+        <div>
           <ButtonGroup>
-            <Button size="M" disabled={false} button_shape= 'semi-rounded-left-to-right' text="Click Me"   position="center_center" icons={{leading_icon: 'golf', trailing_icon: 'archive'}} border={1} font_param={{font:inter, weight:400, style:'normal'}} shadow='shadow-2xl-down' badge='3'/>
+            <Button size="M" disabled={false} button_shape= 'semi-rounded-left-to-right' text="Click Me"   position="center_center" icons={{leading_icon: 'golf', trailing_icon: 'archive'}} border={1} badge='3'/>
             <Button size="M" disabled={false} button_shape= 'semi-rounded-right-to-left' icons={{leading_icon: 'golf'}} border={1} />
           </ButtonGroup>
         </div>
 
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+        <div>
           <ButtonGroup>
-            <Button size="S" disabled={false} button_shape="semi-rounded-left-to-right" text="Click Me" position="center_center" icons={{leading_icon: 'golf', trailing_icon: 'archive'}} border={1} font_param={{font:inter, weight:400, style:'normal'}} shadow='shadow-2xl-down' badge='3'/>
+            <Button size="S" disabled={false} button_shape="semi-rounded-left-to-right" text="Click Me" position="center_center" icons={{leading_icon: 'golf', trailing_icon: 'archive'}} border={1} badge='3'/>
             <Button size="S" disabled={false} button_shape="semi-rounded-right-to-left" icons={{leading_icon: 'globe'}} border={1} />
           </ButtonGroup>
         </div>
 
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+        <div>
           <ButtonGroup>
-            <Button size="XS" disabled={false} button_shape="semi-rounded-left-to-right" text="Click Me"   position="center_center" icons={{leading_icon: 'golf', trailing_icon: 'archive'}} border={1} font_param={{font:inter, weight:400, style:'normal'}} shadow='shadow-2xl-down' badge='3'/>
-            <Button size="XS" disabled={false} button_shape="semi-rounded-right-to-left" icons={{leading_icon: 'golf'}} border={1} />
+            <Button style='Primary' size="XS" disabled={false} button_shape="semi-rounded-left-to-right" text="Click Me"   position="center_center" icons={{leading_icon: 'golf', trailing_icon: 'archive'}} border={1} badge='3'/>
+            <Button style='Primary' size="XS" disabled={false} button_shape="semi-rounded-right-to-left" icons={{leading_icon: 'golf'}} border={1} />
           </ButtonGroup>
         </div>
 
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <Button border={1} button_shape="semi-rounded" icons={{leading_icon: 'check'}} text="Valider" />
+        <div>
+          <Button size="M" style='Primary' border={1} button_shape="semi-rounded" icons={{leading_icon: 'check'}} text="Valider"/>
         </div>
 
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <Button border={1} button_shape="semi-rounded" icons={{leading_icon: 'sliders_horizontal'}} text="Filtrer" badge="3"/>
+        <div>
+          <Button size="M" style='Secondary' disabled={false} border={1} button_shape="semi-rounded" icons={{leading_icon: 'sliders_horizontal'}} text="Filtrer" badge="3"/>
         </div>
 
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+        <div>
           <ButtonGroup>
-            <Button border={1} button_shape="semi-rounded-left-to-right" text="Supprimer" />
-            <Button border={1} button_shape="semi-rounded-right-to-left" icons={{leading_icon: 'gear_six'}} />
+            <Button size="M" style='Destructive' disabled={false} border={1} button_shape="semi-rounded-left-to-right" text="Supprimer" />
+            <Button size="M" style='Destructive' disabled={false} border={1} button_shape="semi-rounded-right-to-left" icons={{leading_icon: 'gear_six'}}/>
           </ButtonGroup>        
         </div>
 
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <Button border={1} button_shape="semi-rounded" icons={{leading_icon: 'trash'}} />
+        <div>
+          <Button_icon style='Secondary' disabled={false} button_shape="semi-rounded" icon='trash' />
         </div>
         
       </main>
