5fe4543a77987742f1432ddd226f43f4e7f892c2
Arthur Jarreau <arthur.jarreau@epitech.eu>
2026-09-14 11:15:04 +0200

    [FIX] only import icon used

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,15 +1,17 @@
-import Button, {Button_icon, ButtonGroup, } from "./component/button";
+'use client'
+import Button, {Button_icon, ButtonGroup} from "./component/button";
+
+import {CheckIcon, SlidersHorizontalIcon, GearSixIcon, TrashIcon} from "@phosphor-icons/react";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
       <main className="flex flex-col gap-25 py-15 px-15 bg-[#f5f5f5] w-full md:w-auto flex-wrap" >
         <div className="flex flex-row gap-5 flex-wrap">
-          <Button size="M" style='Primary' text="Valider" badge='3' icons={{leading_icon: 'Check'}} />
-          <Button size="M" style='Secondary' text="Filtrer" badge='3' icons={{leading_icon: 'SlidersHorizontal'}} />
-          <ButtonGroup size="M" text="Supprimer" style="Destructive" addon_icon="GearSix"/>
-          <Button_icon icon="Trash" style="Secondary"/>
-
+          <Button size="M" style='Primary' text="Valider" badge='3' icons={{leading_icon: CheckIcon}} />
+          <Button size="M" style='Secondary' text="Filtrer" icons={{leading_icon: SlidersHorizontalIcon}} />
+          <ButtonGroup size="M" text="Supprimer" style="Destructive" addon_icon={GearSixIcon}/>
+          <Button_icon icon={TrashIcon} style="Secondary"/>
         </div>
       </main>
     </div>
--- nextjs-dashboard/app/props/get_icons.tsx
+++ nextjs-dashboard/app/props/get_icons.tsx
@@ -1,144 +1,14 @@
 'use client'
-import {
-    Archive, ArrowCounterClockwise, ArrowDown, ArrowLeft, ArrowRight, ArrowSquareIn, ArrowUpRight, ArrowUp,
-    At, Barcode, Basket, BellSimple, BookBookmark, BuildingApartment, BuildingOffice, Building, Buildings,
-    CalendarDots, CameraSlash, Camera,
-    CaretDown, CaretLeft, CaretRight, CaretUpDown, CaretUp,
-    ChartBar, ChartPie, ChatText, Chats,
-    CheckCircle, Check, Checks,
-    ClockCounterClockwise, Clock, CompassTool, CreditCard,
-    Database, DotsThreeVertical, DotsThree, DownloadSimple,
-    Envelope, Export, EyeSlash, Eye,
-    FileText, FlagBannerFold, FlagBanner, FlagCheckered, FlagPennant, Flag,
-    Gauge, GearSix, Gift, Globe, Golf, GpsFix, Gps,
-    Heart, House,
-    IdentificationCard, Info,
-    Lifebuoy, Lightning, Link, ListBullets, List,
-    MagnifyingGlass, MapPinArea, MapPin, MinusCircle, Minus,
-    Notches, Notepad,
-    Package, PaperPlaneTilt, Paperclip, Password,
-    PencilSimple, Phone, Placeholder, PlusCircle, Plus,
-    Printer, ProhibitInset, Prohibit, Pulse,
-    Question,
-    Receipt, Rows,
-    Scales, Scan, ShieldCheck, ShoppingCart, SidebarSimple,
-    SignIn, SignOut, SlidersHorizontal, Sliders, Sparkle, StackSimple, Stack, Star,
-    Suitcase, Table, TagChevron, TagSimple, Tag,
-    TextAlignLeft, TextB, TextItalic, TextUnderline,
-    Ticket, TrafficCone, Trash, TrendDown, TrendUp, Trophy,
-    UploadSimple, User, UsersThree, Users,
-    WarningCircle, WarningDiamond, WarningOctagon, Warning,
-    XCircle, X,
-} from "@phosphor-icons/react";
 
 import styles from "@/src/ui/styles.module.scss"
 
-    export type Icons_name = 'none' | 'Archive' | 
-                'ArrowCounterClockwise' | 'ArrowDown' | 'ArrowLeft' | 'ArrowRight' | 'ArrowSquareIn' | 'ArrowUpRight' | 'ArrowUp' | 
-                'At' | 'Barcode' | 'Basket' | 'BellSimple' | 'BookBookmark' | 'BuildingApartment' | 'BuildingOffice' | 'Building' | 'Buildings' | 
-                'CalendarDots' | 'CameraSlash' | 'Camera' | 
-                'CaretDown' | 'CaretLeft' | 'CaretRight' | 'CaretUpDown' | 'CaretUp' | 
-                'ChartBar' | 'ChartPie' | 'ChatText' | 'Chats' | 
-                'CheckCircle' | 'Check' | 'Checks' | 
-                'ClockCounterClockwise' | 'Clock' | 'CompassTool' | 'CreditCard' | 
-                'Database' | 'DotsThreeVertical' | 'DotsThree' | 'DownloadSimple' | 
-                'Envelope' | 'Export' | 'EyeSlash' | 'Eye' | 
-                'FileText' | 'FlagBannerFold' | 'FlagBanner' | 'FlagCheckered' | 'FlagPennant' | 'Flag' | 
-                'Gauge' | 'GearSix' | 'Gift' | 'Globe' | 'Golf' | 'GpsFix' | 'Gps' | 
-                'Heart' | 'House' | 
-                'IdentificationCard' | 'Info' | 
-                'Lifebuoy' | 'Lightning' | 'Link' | 'ListBullets' | 'List' | 
-                'MagnifyingGlass' | 'MapPinArea' | 'MapPin' | 'MinusCircle' | 'Minus' | 
-                'Notches' | 'Notepad' | 
-                'Package' | 'PaperPlaneTilt' | 'Paperclip' | 'Password' | 
-                'PencilSimple' | 'Phone' | 'Placeholder' | 'PlusCircle' | 'Plus' | 
-                'Printer' | 'ProhibitInset' | 'Prohibit' | 'Pulse' | 
-                'Question' | 
-                'Receipt' | 'Rows' | 
-                'Scales' | 'Scan' | 'ShieldCheck' | 'ShoppingCart' | 'SidebarSimple' | 
-                'SignIn' | 'SignOut' | 'SlidersHorizontal' | 'Sliders' | 'Sparkle' | 'StackSimple' | 'Stack' | 'Star' | 'Suitcase' | 
-                'Table' | 'TagChevron' | 'TagSimple' | 'Tag' | 
-                'TextAlignLeft' | 'TextB' | 'TextItalic' | 'TextUnderline' | 
-                'Ticket' | 'TrafficCone' | 'Trash' | 'TrendDown' | 'TrendUp' | 'Trophy' | 
-                'UploadSimple' | 'User' | 'UsersThree' | 'Users' | 
-                'WarningCircle' | 'WarningDiamond' | 'WarningOctagon' | 'Warning' | 
-                'XCircle' | 'X';
-
 type Draw_Icons_Props = {
-    icon?: Icons_name
+    icon?: any
     size?: number
 }
 
 export default function Draw_Icons({ icon, size }: Draw_Icons_Props) {
-    let Icon : any = undefined;
-    const list_Icons : [Icons_name[], any[]] = [[
-                'none' , 'Archive' , 
-                'ArrowCounterClockwise' , 'ArrowDown' , 'ArrowLeft' , 'ArrowRight' , 'ArrowSquareIn' , 'ArrowUpRight' , 'ArrowUp' , 
-                'At' , 'Barcode' , 'Basket' , 'BellSimple' , 'BookBookmark' , 'BuildingApartment' , 'BuildingOffice' , 'Building' , 'Buildings' , 
-                'CalendarDots' , 'CameraSlash' , 'Camera' , 
-                'CaretDown' , 'CaretLeft' , 'CaretRight' , 'CaretUpDown' , 'CaretUp' , 
-                'ChartBar' , 'ChartPie' , 'ChatText' , 'Chats' , 
-                'CheckCircle' , 'Check' , 'Checks' , 
-                'ClockCounterClockwise' , 'Clock' , 'CompassTool' , 'CreditCard' , 
-                'Database' , 'DotsThreeVertical' , 'DotsThree' , 'DownloadSimple' , 
-                'Envelope' , 'Export' , 'EyeSlash' , 'Eye' , 
-                'FileText' , 'FlagBannerFold' , 'FlagBanner' , 'FlagCheckered' , 'FlagPennant' , 'Flag' , 
-                'Gauge' , 'GearSix' , 'Gift' , 'Globe' , 'Golf' , 'GpsFix' , 'Gps' , 
-                'Heart' , 'House' , 
-                'IdentificationCard' , 'Info' , 
-                'Lifebuoy' , 'Lightning' , 'Link' , 'ListBullets' , 'List' , 
-                'MagnifyingGlass' , 'MapPinArea' , 'MapPin' , 'MinusCircle' , 'Minus' , 
-                'Notches' , 'Notepad' , 
-                'Package' , 'PaperPlaneTilt' , 'Paperclip' , 'Password' , 
-                'PencilSimple' , 'Phone' , 'Placeholder' , 'PlusCircle' , 'Plus' , 
-                'Printer' , 'ProhibitInset' , 'Prohibit' , 'Pulse' , 
-                'Question' , 
-                'Receipt' , 'Rows' , 
-                'Scales' , 'Scan' , 'ShieldCheck' , 'ShoppingCart' , 'SidebarSimple' , 
-                'SignIn' , 'SignOut' , 'SlidersHorizontal' , 'Sliders' , 'Sparkle' , 'StackSimple' , 'Stack' , 'Star' , 'Suitcase' , 
-                'Table' , 'TagChevron' , 'TagSimple' , 'Tag' , 
-                'TextAlignLeft' , 'TextB' , 'TextItalic' , 'TextUnderline' , 
-                'Ticket' , 'TrafficCone' , 'Trash' , 'TrendDown' , 'TrendUp' , 'Trophy' , 
-                'UploadSimple' , 'User' , 'UsersThree' , 'Users' , 
-                'WarningCircle' , 'WarningDiamond' , 'WarningOctagon' , 'Warning' , 
-                'XCircle' , 'X'],
-                [`none`,
-                Archive, ArrowCounterClockwise, ArrowDown, ArrowLeft, ArrowRight, ArrowSquareIn, ArrowUpRight, ArrowUp,
-                At, Barcode, Basket, BellSimple, BookBookmark, BuildingApartment, BuildingOffice, Building, Buildings,
-                CalendarDots, CameraSlash, Camera,
-                CaretDown, CaretLeft, CaretRight, CaretUpDown, CaretUp,
-                ChartBar, ChartPie, ChatText, Chats,
-                CheckCircle, Check, Checks,
-                ClockCounterClockwise, Clock, CompassTool, CreditCard,
-                Database, DotsThreeVertical, DotsThree, DownloadSimple,
-                Envelope, Export, EyeSlash, Eye,
-                FileText, FlagBannerFold, FlagBanner, FlagCheckered, FlagPennant, Flag,
-                Gauge, GearSix, Gift, Globe, Golf, GpsFix, Gps,
-                Heart, House,
-                IdentificationCard, Info,
-                Lifebuoy, Lightning, Link, ListBullets, List,
-                MagnifyingGlass, MapPinArea, MapPin, MinusCircle, Minus,
-                Notches, Notepad,
-                Package, PaperPlaneTilt, Paperclip, Password,
-                PencilSimple, Phone, Placeholder, PlusCircle, Plus,
-                Printer, ProhibitInset, Prohibit, Pulse,
-                Question,
-                Receipt, Rows,
-                Scales, Scan, ShieldCheck, ShoppingCart, SidebarSimple,
-                SignIn, SignOut, SlidersHorizontal, Sliders, Sparkle, StackSimple, Stack, Star,
-                Suitcase, Table, TagChevron, TagSimple, Tag,
-                TextAlignLeft, TextB, TextItalic, TextUnderline,
-                Ticket, TrafficCone, Trash, TrendDown, TrendUp, Trophy,
-                UploadSimple, User, UsersThree, Users,
-                WarningCircle, WarningDiamond, WarningOctagon, Warning,
-                XCircle, X,
-                ]]
-        for (let i = 0; i != list_Icons[0].length; i++) {
-            if (list_Icons[0][i] === icon) {
-                Icon = list_Icons[1][i];
-                break;
-            }
-        }
+    let Icon : any = icon;
         if (icon != 'none' && icon != undefined && Icon != null) {
             return(
             <Icon className={`${styles["Icon"]}`} size={size}/>
